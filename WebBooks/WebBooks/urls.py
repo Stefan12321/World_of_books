d63002982f7fa8bf55a1dfae40f7ccf4755ee77a
Stefan <denik1999@ukr.net>
2022-02-02 07:08:28 +0200

    Added login page

--- WebBooks/WebBooks/urls.py
+++ WebBooks/WebBooks/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from catalog import views
+from django.urls import include, re_path
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('test/', views.test),
+    path('accounts/', include('django.contrib.auth.urls')),
+    re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    re_path(r'^authors/$', views.AuthorListView.as_view(), name='authors')
 ]
--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -1,6 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from .models import Book, Author, BookInstance, Genre
 
 
 def index(request):
-    return HttpResponse("Main page")
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact=2).count()
+    num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+    return render(request, 'index.html', context={'num_books': num_books,
+                                                  'num_instances': num_instances,
+                                                  'num_instances_available': num_instances_available,
+                                                  'num_authors': num_authors,
+                                                  'num_visits': num_visits})
+
+
+def reset_password(request):
+    if request.method == "POST":
+        print('reset password')
+    return render(request, 'registration/reset_password.html')
+
+
+def test(request):
+    return render(request, 'Dashboard Template · Bootstrap v5.1.html')
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 3
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 4
